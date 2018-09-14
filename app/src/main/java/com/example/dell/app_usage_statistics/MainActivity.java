15359c12d4928a3d6d94a8357922c6535ac799d4
arpit7714 <simtmbd@gmail.com>
2018-09-14 19:30:35 +0530

    start project

---
+++ app/src/main/java/com/example/dell/app_usage_statistics/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dell.app_usage_statistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

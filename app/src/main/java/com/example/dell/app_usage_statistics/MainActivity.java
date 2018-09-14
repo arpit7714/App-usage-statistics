98b5760326345dc71306a655a71dd60bfbb6d19a
arpit7714 <simtmbd@gmail.com>
2018-09-14 19:58:16 +0530

    change mainactivity

--- app/src/main/java/com/example/dell/app_usage_statistics/MainActivity.java
+++ app/src/main/java/com/example/dell/app_usage_statistics/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.dell.app_usage_statistics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AbsListView;
+import android.app.ActionBar;
+import android.app.AppOpsManager;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.location.SettingInjectorService;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.transition.Fade;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressbar;
+    private TextView  permissionMessage;
+    private Button button;
+    //total usage time
+    private long mtotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        progressbar = (ProgressBar) findViewById(R.id.progress_bar);
+        permissionMessage = (TextView) findViewById(R.id.grant_permission_message);
+        button =(Button) findViewById(R.id.permissionbutton);
+
     }
 }
